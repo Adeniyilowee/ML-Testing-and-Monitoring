ca548a587e4e58cfa0dc46b36e172f8d8914bb71
adeniyim <mosakuadeniyi@gmail.com>
2024-08-16 12:27:55 +0200

    Updated to bagging

--- src/lsp_model/model.py
+++ src/lsp_model/model.py
@@ -1,7 +1,5 @@
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -10,19 +8,7 @@ class Model_Choice(BaseEstimator, TransformerMixin):
 
     def __init__(self, loss, random_state, n_estimators, modelchoice):
 
-        if modelchoice == "svm":
-            self.model = SVC(C=113, gamma=0.12, kernel='rbf', probability=True)
-
-        elif modelchoice == "boosting_dt":
-
-            classifier = DecisionTreeClassifier(ccp_alpha=0.0, criterion='entropy',
-                                                min_samples_leaf=1, min_samples_split=4,
-                                                min_weight_fraction_leaf=0.0, splitter='best')
-
-            self.model = AdaBoostClassifier(base_estimator=classifier, n_estimators=n_estimators,
-                                            random_state=random_state)
-
-        elif modelchoice == "bagging_dt":
+        if modelchoice == "bagging_dt":
 
             classifier = DecisionTreeClassifier(ccp_alpha=0.0, criterion='entropy',
                                                 min_samples_leaf=1, min_samples_split=4,
